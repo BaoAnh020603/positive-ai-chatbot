d1642a50f1a5cfdba84fe388b024461eef690380
Nguyễn Nhật Bảo Anh <143186266+BaoAnh020603@users.noreply.github.com>
2025-06-02 10:19:07 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import PositiveAIChatbot from './components/PositiveAIChatbot';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <PositiveAIChatbot />
+    </div>
+  );
+}
+
+export default App;
